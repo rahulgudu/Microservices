4201eb16e88f271c35edccece79bfc7a3cecf54a
sushantskb <sushantsrk2001@gmail.com>
2025-12-04 13:33:23 +0530

    created query service

---
+++ query/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get("/posts", (req, res) => {});
+
+app.post("/events", (req, res) => {});
+
+app.listen(6000, () => console.log("Listening on 6000"));
