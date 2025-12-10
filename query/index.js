7630195d4625ee9e882717cd5ffc844d1f641e84
sushantskb <sushantsrk2001@gmail.com>
2025-12-10 12:56:51 +0530

    parsing incoming events

--- query/index.js
+++ query/index.js
@@ -3,12 +3,32 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const app = express();
+const posts = {};
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { postId, content, id } = data;
+    const post = posts[postId];
+
+    post.comments.push({ id, content });
+  }
+  console.log(posts);
+
+  res.send({});
+});
 
 app.listen(6000, () => console.log("Listening on 6000"));
