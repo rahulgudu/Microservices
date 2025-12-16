0538eac51035c5ff4ffd3d4268b23c28d7ca9a4a
sushantskb <sushantsrk2001@gmail.com>
2025-12-16 21:09:41 +0530

    updating the comment content

--- query/index.js
+++ query/index.js
@@ -21,10 +21,10 @@ app.post("/events", (req, res) => {
   }
 
   if (type === "CommentCreated") {
-    const { postId, content, id } = data;
+    const { postId, content, id, status } = data;
     const post = posts[postId];
 
-    post.comments.push({ id, content });
+    post.comments.push({ id, content, status });
   }
   console.log(posts);
 
